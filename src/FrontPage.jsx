6fbc0e1ca93b2796ba18ddf50a66da2ae23e53f1
ProdDestroyer <samuelfelipelopez@gmail.com>
2026-08-04 00:07:31 -0500

    Footer added

--- src/FrontPage.jsx
+++ src/FrontPage.jsx
@@ -1,11 +1,13 @@
 import Navbar from './NavBar'
 import { Home } from './Home'
+import { Footer } from './Footer'
 
 export const FrontPage = () => {
   return (
     <>
         <Navbar></Navbar>
         <Home></Home>
+        <Footer></Footer>
     </>
   )
 }
