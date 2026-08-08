65d7a806a8222b211fac6e409192eae8cab71c4c
ProdDestroyer <samuelfelipelopez@gmail.com>
2026-08-08 14:33:47 -0500

    fixing logo blikning transition on lineView attempt 24

--- src/LineView.jsx
+++ src/LineView.jsx
@@ -60,33 +60,33 @@ export const LineView = () => {
   const imageRef = useRef(null);
   const [showNavbarLogo, setShowNavbarLogo] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (!imageRef.current) return;
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (!imageRef.current) return;
 
-      const rect = imageRef.current.getBoundingClientRect();
+  //     const rect = imageRef.current.getBoundingClientRect();
 
-      // const shouldShow = rect.bottom <= 60;
+  //     // const shouldShow = rect.bottom <= 60;
 
-      // setShowNavbarLogo(prev =>
-      //   prev === shouldShow ? prev : shouldShow
-      // );
-      setShowNavbarLogo(false);
-    };
+  //     // setShowNavbarLogo(prev =>
+  //     //   prev === shouldShow ? prev : shouldShow
+  //     // );
+  //     setShowNavbarLogo(false);
+  //   };
 
-    window.addEventListener("scroll", handleScroll, { passive: true });
+  //   window.addEventListener("scroll", handleScroll, { passive: true });
 
-    handleScroll();
+  //   handleScroll();
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
 
-  const scrollTop = () => {
-    window.scrollTo(0, 0);
-    console.log('scrolled top');
-  }
+  // const scrollTop = () => {
+  //   window.scrollTo(0, 0);
+  //   console.log('scrolled top');
+  // }
 
 //   useEffect(() => {
 //     console.log("Before:", window.scrollY);
