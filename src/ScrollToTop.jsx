8305b817e7b4a09d4b14982d5bb3a235ca77171e
ProdDestroyer <samuelfelipelopez@gmail.com>
2026-08-08 14:40:44 -0500

    fixing logo blikning transition on lineView attempt 27

--- src/ScrollToTop.jsx
+++ src/ScrollToTop.jsx
@@ -5,8 +5,6 @@ export const ScrollToTop = () => {
     const { pathname } = useLocation();
 
     useLayoutEffect(() => {
-        window.history.scrollRestoration = "manual";
-
         window.scrollTo(0, 0);
         document.documentElement.scrollTop = 0;
         document.body.scrollTop = 0;
