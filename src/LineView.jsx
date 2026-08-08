25a2a42b03c9f4e5a0d6888ad238202dca5e868b
ProdDestroyer <samuelfelipelopez@gmail.com>
2026-08-08 10:28:37 -0500

    fixing logo blikning transition on lineView attempt 16

--- src/LineView.jsx
+++ src/LineView.jsx
@@ -108,19 +108,21 @@ export const LineView = () => {
           <span>{navBarTitle}</span>
         </div>} */}
         <div
-          className={`${styles.navBarImageContainer} ${showNavbarLogo
-              ? styles.navBarLogoVisible
-              : styles.navBarLogoHidden
-            }`}
-        >
-          <div
-            className={styles.navBarLogoContainer}
-            onClick={() => navigate("/")}
-          >
-            <img src="/goldenLogo.png" alt="" />
-            <span>{navBarTitle}</span>
-          </div>
-        </div>
+    className={`${styles.navBarImageContainer} ${
+        showNavbarLogo
+            ? styles.navBarLogoVisible
+            : styles.navBarLogoHidden
+    }`}
+>
+    <div
+        className={styles.navBarLogoContainer}
+        onClick={() => navigate("/")}
+    >
+        <img src="/goldenLogo.png" alt="" />
+    </div>
+
+    <span>{navBarTitle}</span>
+</div>
         {!showNavbarLogo &&
           <span>{navBarTitle}</span>}
       </div>
