41a1a4556f43bddc4b168c95a0c50fd023a77f44
ProdDestroyer <samuelfelipelopez@gmail.com>
2026-08-10 01:23:13 -0500

    fixing simple nav bar component name 2

--- src/SimpleTopBar.jsx
+++ src/SimpleTopBar.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./SimpleNavbar.module.css";
 
-export const SimpleNavbar = ({ title }) => {
+export const SimpleTopbar = ({ title }) => {
 
     const navigate = useNavigate();
 
